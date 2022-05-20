1d3d4114fd51e2794f11c6ffc7532b7492123e86
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-05-20 04:06:35 +0200

    Bouncer sprite

--- Assets/Tarodev 2D Controller/Demo/Demo Scripts/Bouncer.cs
+++ Assets/Tarodev 2D Controller/Demo/Demo Scripts/Bouncer.cs
@@ -3,12 +3,28 @@ using TarodevController;
 using UnityEngine;
 
 namespace Tarodev {
-    public class Bouncer : MonoBehaviour {
+    public class Bouncer : MonoBehaviour 
+    {
         [SerializeField] private float _bounceForce = 70;
+        private Animator _anim;
 
-        private void OnCollisionStay2D(Collision2D other) {
+        private void Awake()
+        {
+            _anim = GetComponent<Animator>();
+        }
+
+        private void OnCollisionStay2D(Collision2D other) 
+        {
             if (other.collider.TryGetComponent(out IPlayerController controller)) {
                 controller.AddForce(transform.up.normalized * _bounceForce);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                _anim.Play("BouncerAnim");
             }
         }
     }
