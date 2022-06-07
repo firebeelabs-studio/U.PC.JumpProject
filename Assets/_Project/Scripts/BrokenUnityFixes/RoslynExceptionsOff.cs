2a7ff038612c33c30589d761353c015ad0b29888
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-06-07 08:48:57 +0200

    Playflow setup v2

--- Assets/_Project/Scripts/BrokenUnityFixes/RoslynExceptionsOff.cs
+++ Assets/_Project/Scripts/BrokenUnityFixes/RoslynExceptionsOff.cs
@@ -1,28 +1,27 @@
 using System.IO;
 using System.Text.RegularExpressions;
-//using UnityEditor;
-//using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
 
-//[InitializeOnLoad]
+[InitializeOnLoad]
 public static class RoslynExceptionsOff
 {
-#if UNITY_EDITOR
-    //static RoslynExceptionsOff() => Application.logMessageReceived += OnLogMessageReceived;
+    static RoslynExceptionsOff() => Application.logMessageReceived += OnLogMessageReceived;
 
-    //private static void OnLogMessageReceived(string message, string _, LogType logType)
-    //{
-    //    if (logType != LogType.Exception)
-    //        return;
+    private static void OnLogMessageReceived(string message, string _, LogType logType)
+    {
+        if (logType != LogType.Exception)
+            return;
 
-    //    const string pattern =
-    //                @"^DirectoryNotFoundException: Could not find " +
-    //                @"a part of the path ('|"")Temp(\\|/)RoslynAnalysisRunner";
+        const string pattern =
+                    @"^DirectoryNotFoundException: Could not find " +
+                    @"a part of the path ('|"")Temp(\\|/)RoslynAnalysisRunner";
 
-    //    if (Regex.IsMatch(message, pattern))
-    //    {
-    //        Directory.CreateDirectory("Temp/RoslynAnalysisRunner");
-    //    }
-    //}
-#endif
+        if (Regex.IsMatch(message, pattern))
+        {
+            Directory.CreateDirectory("Temp/RoslynAnalysisRunner");
+        }
+    }
 }
+#endif
