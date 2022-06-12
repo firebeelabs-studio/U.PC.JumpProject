f2ef5eb29d2b06b6570ece8ed649ece099ce7aae
Matitek <mpawelczak87@gmail.com>
2022-06-12 14:57:15 +0200

    Parallax Background
    code changed

--- Assets/_Project/Scripts/Cameras/ParallaxBackground.cs
+++ Assets/_Project/Scripts/Cameras/ParallaxBackground.cs
@@ -6,23 +6,28 @@ public class ParallaxBackground : MonoBehaviour
 {
     private float _length;
     private float _startpos;
-    [SerializeField] float parallaxEffect;
-    [SerializeField] GameObject Cam;
+    [SerializeField] private float _parallaxEffect;
+    [SerializeField] private GameObject _cam;
 
-    void Start()
+    void Awake()
     {
         _startpos = transform.position.x;
         _length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        float _temp = Cam.transform.position.x * (1 - parallaxEffect);
-        float _dist = (Cam.transform.position.x * parallaxEffect);
+        float _temp = _cam.transform.position.x * (1 - _parallaxEffect);
+        float _dist = (_cam.transform.position.x * _parallaxEffect);
         transform.position = new Vector3(_startpos + _dist, transform.position.y, transform.position.z);
 
-        if (_temp > _startpos + _length) _startpos += _length;
-        else if (_temp < _startpos - _length) _startpos -= _length;
+        if (_temp > _startpos + _length)
+        {
+            _startpos += _length;
+        }
+        else if (_temp < _startpos - _length)
+        {
+            _startpos -= _length;
+        }
     }
 }
