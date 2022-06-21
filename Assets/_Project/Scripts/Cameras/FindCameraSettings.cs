055327917f1fcb957a0677127a400aa5f638eb04
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-06-21 16:45:43 +0200

    Camera settings obj

---
+++ Assets/_Project/Scripts/Cameras/FindCameraSettings.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using FishNet.Object;
+using UnityEngine;
+using Cinemachine;
+
+public class FindCameraSettings : NetworkBehaviour
+{
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        if (base.IsOwner)
+        {
+            GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize = FindObjectOfType<CameraSettings>().CameraSize;
+        }
+    }
+}
