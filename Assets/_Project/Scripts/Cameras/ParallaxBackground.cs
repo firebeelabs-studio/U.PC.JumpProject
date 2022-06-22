70d035eabf2c1f4c2b6cc540d89cf29d1dffb4ae
KrystianKuska <47749622+OsheOse@users.noreply.github.com>
2022-06-22 15:55:52 +0200

    fixes
    comments + class out to the other script

--- Assets/_Project/Scripts/Cameras/ParallaxBackground.cs
+++ Assets/_Project/Scripts/Cameras/ParallaxBackground.cs
@@ -5,15 +5,8 @@ using UnityEngine;
 public class ParallaxBackground : MonoBehaviour
 {
     [SerializeField] private GameObject _cam;
-    [System.Serializable]
-    public class ParallaxBackgroundParts
-    {
-        public GameObject BackgroundPart;
-        public float ParallaxEffectPower;
-        [HideInInspector] public float Length;
-        [HideInInspector] public float StartPos;
-    }
     [SerializeField] private List<ParallaxBackgroundParts> backgroundParts;
+    [SerializeField] private float _offsetForDistance = 3f;
 
     void Awake()
     {
@@ -25,16 +18,16 @@ public class ParallaxBackground : MonoBehaviour
     }
     private void LateUpdate()
     {
-        foreach (ParallaxBackgroundParts part in backgroundParts)
+        foreach (ParallaxBackgroundParts part in backgroundParts)       //loops through all elements in list 'backgroundParts' and executes code below for each of them
         {
-            float _temp = _cam.transform.position.x * (1 - part.ParallaxEffectPower);
-            float _dist = _cam.transform.position.x * part.ParallaxEffectPower - 3f;
-            part.BackgroundPart.transform.position = new Vector3(part.StartPos + _dist, part.BackgroundPart.transform.position.y, part.BackgroundPart.transform.position.z);
-            if (_temp > part.StartPos + part.Length/2)
+            float tempPosOfBgPart = _cam.transform.position.x * (1 - part.ParallaxEffectPower);       //calculates the temporary position of the part depending on parallax effect power relating to the camera position
+            float distance = _cam.transform.position.x * part.ParallaxEffectPower - _offsetForDistance;        //calculates distance which background part will move
+            part.BackgroundPart.transform.position = new Vector3(part.StartPos + distance, part.BackgroundPart.transform.position.y, part.BackgroundPart.transform.position.z);     //moves each part in x axis depending on it's starting position and distance
+            if (tempPosOfBgPart > part.StartPos + part.Length/2)        //if temp pos value is bigger than the starting pos + half of length of sprite teleports the part to make it looping
             {
                 part.StartPos += part.Length;
             }
-            else if (_temp < part.StartPos - part.Length)
+            else if (tempPosOfBgPart < part.StartPos - part.Length)     //same as above but in opposite direction
             {
                 part.StartPos -= part.Length;
             }
---
+++ Assets/_Project/Scripts/Cameras/ParallaxBackgroundParts.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[System.Serializable]                                   //allows us to see list of objects of this class in inspector
+public class ParallaxBackgroundParts
+{
+    public GameObject BackgroundPart;
+    public float ParallaxEffectPower;
+    [HideInInspector] public float Length;
+    [HideInInspector] public float StartPos;
+}
