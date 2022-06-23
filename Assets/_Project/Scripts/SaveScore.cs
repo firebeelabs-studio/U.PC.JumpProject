e96d7cf4be38d687d4890acd6111dac35708c2f7
Matitek <mpawelczak87@gmail.com>
2022-06-23 17:41:10 +0200

    fix/Added SaveScore

--- Assets/_Project/Scripts/SaveScore.cs
+++ Assets/_Project/Scripts/SaveScore.cs
@@ -1,71 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.IO;
 using System.Globalization;
+using UnityEngine;
 
 public class SaveScore : MonoBehaviour
 {
     private Timer _timer;
     bool _isGameStarted;
-    List<TimeData> Players;
-
-    [System.Serializable]
-    private class TimeData
-    {
-        public string WalletNumber = "Player 1";
-        public int MinutesData;
-        public float SecondsData;
-    }
+    List<ScoreData> Players;
     private void Awake()
     {
         _timer = FindObjectOfType<Timer>();
     }
-
     private void Start()
     {
         _isGameStarted = true;
     }
-
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.CompareTag("Player") && _isGameStarted)
+        if (col.CompareTag("Player") && _isGameStarted)
         {
             _isGameStarted = false; //let the player save time only 1 time
-            
             //load data from JSON and turn it into float variable that contains time in seconds
             string stringPreviousRecord = File.ReadAllText(Application.dataPath + "/scoreFile.json");
-            TimeData loadPreviousRecord = JsonUtility.FromJson<TimeData>(stringPreviousRecord);
-            float oldRedordInSeconds = loadPreviousRecord.MinutesData*60 + loadPreviousRecord.SecondsData;
-            
+            ScoreData loadPreviousRecord = JsonUtility.FromJson<ScoreData>(stringPreviousRecord);
+            float oldRecordInSeconds = loadPreviousRecord.MinutesData * 60 + loadPreviousRecord.SecondsData;
+
             //load new time score and turn it into time in seconds
-            float checkNewTime = int.Parse(_timer.minutes)*60 + float.Parse(_timer.seconds, CultureInfo.InvariantCulture.NumberFormat);
+            float checkNewTime = int.Parse(_timer.minutes) * 60 + float.Parse(_timer.seconds, CultureInfo.InvariantCulture.NumberFormat);
 
-            Debug.Log("oldRecordInSeconds" + oldRedordInSeconds);
-            Debug.Log("newScore" + checkNewTime);
-
-            //check if new score is better and save, when its true;
-            if (oldRedordInSeconds < checkNewTime) return;
+            //check if new score is better and save when its true
+            if (oldRecordInSeconds < checkNewTime) return;
             SaveToJson();
-            Debug.Log("Congratulations! Your score is better!");
         }
     }
-
-    public void SaveToJson()
+    private void SaveToJson()
     {
         Players = new();
-        TimeData newScoreData = new TimeData();
+        ScoreData newScoreData = new ScoreData();
         newScoreData.MinutesData = int.Parse(_timer.minutes);
         newScoreData.SecondsData = float.Parse(_timer.seconds, CultureInfo.InvariantCulture.NumberFormat);
         Players.Add(newScoreData);
 
-        TimeData newScoreData2 = new TimeData();
-        newScoreData2.MinutesData = int.Parse(_timer.minutes);
-        newScoreData2.SecondsData = float.Parse(_timer.seconds, CultureInfo.InvariantCulture.NumberFormat);
-        newScoreData2.WalletNumber = "Player 2";
-        Players.Add(newScoreData2);
-
-        string json = JsonUtility.ToJson(Players);
+        string json = JsonUtility.ToJson(newScoreData);
         File.WriteAllText(Application.dataPath + "/scoreFile.json", json);
     }
 }
---
+++ Assets/_Project/Scripts/ScoreData.cs
@@ -0,0 +1,11 @@
+public class ScoreData
+{
+    public string WalletNumber = "Player 1";
+    public int MinutesData;
+    public float SecondsData;
+}
+
+
+
+
+
