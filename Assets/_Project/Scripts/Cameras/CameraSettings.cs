dd9ff5035bad52c17e89c71552890f507eebe1f4
KrystianKuska <47749622+OsheOse@users.noreply.github.com>
2022-06-25 17:25:37 +0200

    fix

--- Assets/_Project/Scripts/Cameras/CameraSettings.cs
+++ Assets/_Project/Scripts/Cameras/CameraSettings.cs
@@ -6,7 +6,16 @@ public class CameraSettings : MonoBehaviour
 {
     public static CameraSettings Instance { get; private set; }
     public float CameraSize;
+    [Space(10)]
     public bool ShouldZoom;
+    [Space(10)]
+    public bool ShouldParallax;
+    public enum ParallaxAxis
+    {
+        Vertical,
+        Horizontal
+    }
+    public ParallaxAxis Axis;
     private void Awake()
     {
         Instance = this;
