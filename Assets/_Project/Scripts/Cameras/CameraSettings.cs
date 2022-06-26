51525893e232a92d60996bb29f4c09a96f02ecdc
5Z3L4 <58202728+5Z3L4@users.noreply.github.com>
2022-06-27 00:20:16 +0200

    Update CameraSettings.cs

--- Assets/_Project/Scripts/Cameras/CameraSettings.cs
+++ Assets/_Project/Scripts/Cameras/CameraSettings.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[Serializable]
+
 public class CameraSettings : MonoBehaviour
 {
     public static CameraSettings Instance { get; private set; }
