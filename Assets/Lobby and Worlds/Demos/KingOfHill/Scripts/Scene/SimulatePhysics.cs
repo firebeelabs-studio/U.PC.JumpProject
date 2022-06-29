717d17d3f548238543434ec69d8a5fec6384a9a9
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-06-29 18:01:52 +0200

    Lobbys and Worlds implementation

---
+++ Assets/Lobby and Worlds/Demos/KingOfHill/Scripts/Scene/SimulatePhysics.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace FirstGearGames.LobbyAndWorld.Demos.KingOfTheHill
+{
+
+
+    /// <summary>
+    /// Simulates phyics for the current scene.
+    /// </summary>
+    public class SimulatePhysics : MonoBehaviour
+    {
+        /// <summary>
+        /// Accumulated physics step.
+        /// </summary>
+        private float _stepTime = 0f;
+        /// <summary>
+        /// PhysicsScene this object is in. Required for scene stacking.
+        /// </summary>
+        private PhysicsScene _physicsScene;
+
+        private void Awake()
+        {
+            /* //Note In 2020+ I believe this is
+             * Physics.SimulationMode = script; */
+            Physics.autoSimulation = false;
+            _physicsScene = gameObject.scene.GetPhysicsScene();
+        }
+
+        private void Update()
+        {
+            float fixedDelta = Time.fixedDeltaTime;
+            _stepTime += Time.deltaTime;
+            while (_stepTime >= fixedDelta)
+            {
+                _stepTime -= fixedDelta;
+                _physicsScene.Simulate(fixedDelta);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Lobby and Worlds/Scripts/Mirror/LocalPlayerAnnouncer.cs
@@ -0,0 +1,54 @@
+﻿
+using FishNet.Connection;
+using FishNet.Object;
+using System;
+
+
+namespace FirstGearGames.Mirrors
+{
+
+    /// <summary>
+    /// To be attached to localPlayer prefabs to know when they are instantiated.
+    /// </summary>
+    public class LocalPlayerAnnouncer : NetworkBehaviour
+    {
+
+        #region Public.
+        /// <summary>
+        /// Dispatched when the local player changes, providing the new localPlayer.
+        /// </summary>
+        public static event Action<NetworkObject> OnLocalPlayerUpdated;
+        #endregion
+
+        #region Start/Destroy
+        public override void OnOwnershipClient(NetworkConnection prevOwner)
+        {
+            base.OnOwnershipClient(prevOwner);
+            if (base.IsOwner)
+                OnLocalPlayerUpdated?.Invoke(base.NetworkObject);
+        }
+
+        //private void OnDestroy()
+        //{
+        //    if (base.IsOwner)
+        //        OnLocalPlayerUpdated?.Invoke(null);
+        //}
+        #endregion
+
+        #region OnEnable/Disable.
+        private void OnEnable()
+        {
+            if (base.IsOwner)
+                OnLocalPlayerUpdated?.Invoke(base.NetworkObject);
+        }
+        private void OnDisable()
+        {
+            if (base.IsOwner)
+                OnLocalPlayerUpdated?.Invoke(null);
+        }
+        #endregion
+
+    }
+
+
+}
