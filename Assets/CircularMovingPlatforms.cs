d4ecfb3e995e4edb3585472c06f9dee5feb385b3
KrystianKuska <47749622+OsheOse@users.noreply.github.com>
2022-07-08 18:12:26 +0200

    circular movement platforms

---
+++ Assets/CircularMovingPlatforms.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMovingPlatforms : MonoBehaviour
+{
+    [SerializeField] private float _radius;
+    [SerializeField]private float _speed;
+    [SerializeField] private GameObject[] _platforms;
+    private float _angleStep;
+
+    private void Start()
+    {
+        _angleStep = Mathf.PI * 2 / _platforms.Length; //dividing full circle angles (radians) by number of platforms
+        for (int i = 0; i < _platforms.Length; i++)
+        {
+            float angle = i * _angleStep; //setting the starting angle depending on index
+            Vector2 newPos = new Vector2(transform.position.x + Mathf.Cos(angle) * _radius, transform.position.y + Mathf.Sin(angle) * _radius); //creating new vector for each platform's position
+            _platforms[i].transform.position = newPos;
+            if (_platforms[i].TryGetComponent<PlatformCircularEffector>(out PlatformCircularEffector effector))
+            {
+                effector.Angle = angle;
+                effector.Radius = _radius;
+                effector.Speed = _speed;
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, _radius);
+    }
+}
---
+++ Assets/PlatformCircularEffector.cs
@@ -0,0 +1,37 @@
+using TarodevController;
+using UnityEngine;
+
+public class PlatformCircularEffector : MonoBehaviour, IPlayerEffector
+{
+    public float Angle;
+    public float Radius;
+    public float Speed;
+    [SerializeField] private GameObject _axisObj;
+    private Rigidbody2D _rb;
+    private Vector2 _change, _lastPos, _nextPos;
+    private float _nextPosX, _nextPosY;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        Angle += Time.fixedDeltaTime * Speed; //increasing angle value
+        
+        _nextPosX = _axisObj.transform.position.x + Mathf.Cos(Angle) * Radius; //calculating new x position around the axis (parent object)
+        _nextPosY = _axisObj.transform.position.y + Mathf.Sin(Angle) * Radius; //calculating new y position around the axis (parent object)
+        _nextPos = new(_nextPosX, _nextPosY); //applying the new position
+        _rb.MovePosition(_nextPos);
+        if (Angle >= 360f)
+        {
+            Angle = 0;
+        }
+        _change = _lastPos - _nextPos; //calculating the difference between last position and next position
+        _lastPos = _nextPos;
+    }
+    public Vector2 EvaluateEffector()
+    {
+        return -_change;
+    }
+}
