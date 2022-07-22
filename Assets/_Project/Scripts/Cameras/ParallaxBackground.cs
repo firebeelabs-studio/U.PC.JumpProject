4062b512b7226315d9e599e533317253ba2b8152
Krystian Kuska <47749622+OsheOse@users.noreply.github.com>
2022-07-23 01:39:58 +0200

    new parallax bg + shiny flies + clouds

--- Assets/_Project/Scripts/Cameras/ParallaxBackground.cs
+++ Assets/_Project/Scripts/Cameras/ParallaxBackground.cs
@@ -20,25 +20,33 @@ public class ParallaxBackground : MonoBehaviour
     }
     private void Parallax(bool axis)
     {
-        var parallaxAxis = axis == true ? _parallaxReferenceObject.transform.position.x : _parallaxReferenceObject.transform.position.y;
+        float parallaxAxis = axis == true ? _parallaxReferenceObject.transform.position.x : _parallaxReferenceObject.transform.position.y;
         if (!CameraSettings.Instance.ShouldParallax) return;
-        foreach (ParallaxBackgroundParts part in backgroundParts)       //loops through all elements in list 'backgroundParts' and executes code below for each of them
+
+        //loops through all elements in list 'backgroundParts' and executes code below for each of them
+        foreach (ParallaxBackgroundParts part in backgroundParts)
         {
-            float tempPosOfBgPart = parallaxAxis * (1 - part.ParallaxEffectPower);       //calculates the temporary position of the part depending on parallax effect power relating to the camera position
-            float distance = parallaxAxis * part.ParallaxEffectPower ;//- _offsetForDistance;        //calculates distance which background part will move
+
+            //calculates the temporary position of the part depending on parallax effect power relating to the camera position
+            float tempPosOfBgPart = parallaxAxis * (1 - part.ParallaxEffectPower);
+            //calculates distance which background part will move
+            float distance = parallaxAxis * part.ParallaxEffectPower;
             if(axis)
             {
-                part.BackgroundPart.transform.position = new Vector3(part.StartPos + distance , part.BackgroundPart.transform.position.y, part.BackgroundPart.transform.position.z);     //moves each part in x axis depending on it's starting position and distance
+                //moves each part in x axis depending on it's starting position and distance
+                part.BackgroundPart.transform.position = new Vector3(part.StartPos + distance , part.BackgroundPart.transform.position.y, part.BackgroundPart.transform.position.z);
             }
             else
             {
                 part.BackgroundPart.transform.position = new Vector3(part.BackgroundPart.transform.position.x, part.StartPos + distance, part.BackgroundPart.transform.position.z);
             }
-            if (tempPosOfBgPart > part.StartPos + part.Length / 2)        //if temp pos value is bigger than the starting pos + half of length of sprite teleports the part to make it looping
+            //if temp pos value is bigger than the starting pos + half of length of sprite teleports the part to make it looping
+            if (tempPosOfBgPart > part.StartPos + part.Length / 2)
             {
                 part.StartPos += part.Length;
             }
-            else if (tempPosOfBgPart < part.StartPos - part.Length)     //same as above but in opposite direction
+            //same as above but in opposite direction
+            else if (tempPosOfBgPart < part.StartPos - part.Length)
             {
                 part.StartPos -= part.Length;
             }
