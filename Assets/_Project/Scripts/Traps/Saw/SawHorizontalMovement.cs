38cb3cf0dd784701aca8fd674084ef1b90aeee98
Adrian Barczewski <adrianbarczewski@gmail.com>
2022-08-06 19:18:06 +0200

    Saw movement adjustment + particles

--- Assets/_Project/Scripts/Traps/Saw/SawHorizontalMovement.cs
+++ Assets/_Project/Scripts/Traps/Saw/SawHorizontalMovement.cs
@@ -8,18 +8,23 @@ public class SawHorizontalMovement : MonoBehaviour
     [SerializeField] private float _speed = 2f;
     private int _currentWaypointIndex = 0;
     [SerializeField] Transform parentTransform;
+    private int _sparkDirection;
 
     private void Update()
     {
         if (Vector2.Distance(_waypoints[_currentWaypointIndex].transform.position, parentTransform.position) < .1f)
         {
             _currentWaypointIndex++;
+            _sparkDirection = -1;
 
             if (_currentWaypointIndex >= _waypoints.Length)
             {
                 _currentWaypointIndex = 0;
+                _sparkDirection = 1;
             }
+            transform.localScale = new Vector3 (_sparkDirection, 1, 1);
         }
         parentTransform.position = Vector2.MoveTowards(parentTransform.position, _waypoints[_currentWaypointIndex].transform.position, Time.deltaTime * _speed);
+        
     }
 }
