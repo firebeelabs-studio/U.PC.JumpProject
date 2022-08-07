fd0a2efe517e9608092c4d453c4fb9b62c5d7ca6
Matitek <mpawelczak87@gmail.com>
2022-08-07 21:38:55 +0200

    reduced Scale

--- Assets/SetFlag.cs
+++ Assets/SetFlag.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class SetFlag : MonoBehaviour
 {
+    [SerializeField] Transform _flag;
     [SerializeField] Transform _top;
     [SerializeField] Transform _mid;
     [SerializeField] float _speed;
@@ -11,16 +12,19 @@ public class SetFlag : MonoBehaviour
     Vector3 _startScale, _endScale;
     bool _didItOnce;
     bool _activateCheckpoint;
+    float _includeScale;
 
     private void Start()
     {
         _endTopPos = (Vector2)_top.position + new Vector2(0, _height);
         _endMidPos = (Vector2)_mid.position + new Vector2(0, _height/2);
 
+        // this includes the scale of main object, 
+        _includeScale = 1/_flag.transform.localScale.y;
         _startScale = _mid.transform.localScale;
 
         //height of scale is increased a bit, to prevent open space between two sprites
-        _endScale = _startScale + new Vector3(0, _height, 0);
+        _endScale = _startScale + new Vector3(0, _includeScale * _height, 0);
     }
 
     private void Update()
@@ -43,6 +47,6 @@ public class SetFlag : MonoBehaviour
         _top.position = Vector2.MoveTowards(_top.position, _endTopPos, _speed * Time.deltaTime);
         _mid.position = Vector2.MoveTowards(_mid.position, _endMidPos, (_speed/2) * Time.deltaTime);
 
-        _mid.transform.localScale = Vector3.MoveTowards(_mid.transform.localScale, _endScale, _speed * Time.deltaTime);
+        _mid.transform.localScale = Vector3.MoveTowards(_mid.transform.localScale, _endScale, _includeScale * _speed * Time.deltaTime);
     }
 }
