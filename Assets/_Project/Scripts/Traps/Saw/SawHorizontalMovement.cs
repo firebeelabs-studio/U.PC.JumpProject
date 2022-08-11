edecff8ef8565f99eed4d9166dbef83772570ea7
Adrian Barczewski <adrianbarczewski@gmail.com>
2022-08-11 17:20:10 +0200

    Saw horizontal movement fixed

--- Assets/_Project/Scripts/Traps/Saw/SawHorizontalMovement.cs
+++ Assets/_Project/Scripts/Traps/Saw/SawHorizontalMovement.cs
@@ -6,10 +6,26 @@ public class SawHorizontalMovement : MonoBehaviour
 {
     [SerializeField] private GameObject[] _waypoints;
     [SerializeField] private float _speed = 2f;
-    private int _currentWaypointIndex = 0;
+    [SerializeField] private int _currentWaypointIndex = 0;
     [SerializeField] Transform parentTransform;
     [SerializeField] private Transform _particleTransform;
     private int _sparkDirection;
+
+    private void Start()
+    {
+        if (_currentWaypointIndex == 0)
+        {
+            _sparkDirection = 1;
+        }
+        else if (_currentWaypointIndex > 0)
+        {
+            _sparkDirection = -1;
+        }
+        transform.localScale = new Vector3 (_sparkDirection, 1, 1);
+        _particleTransform.localScale = new Vector3(_sparkDirection, 1, 1);  
+        parentTransform.position = Vector2.MoveTowards(parentTransform.position, _waypoints[_currentWaypointIndex].transform.position, Time.deltaTime * _speed);
+
+    }
 
     private void Update()
     {
