93a0e66e149f1a4316a3185c236b16a6cc5b2b0c
Adrian Barczewski <adrianbarczewski@gmail.com>
2022-08-15 15:07:53 +0200

    Boulder destroying added

---
+++ Assets/_Project/Scripts/RandomEvents/BoulderActivate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderActivate : MonoBehaviour
+{
+    [SerializeField] private GameObject _boulder;
+    [SerializeField] private Transform _position;
+
+    private void Start()
+    {
+        _boulder.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.name.Equals("Player"))
+        {
+            _boulder.SetActive(true);
+        }
+    }
+}
