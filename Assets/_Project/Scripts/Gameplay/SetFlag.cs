edc494105c4cae7434cf2aeeed7704fc5e2ebfa9
Matitek <mpawelczak87@gmail.com>
2022-08-15 22:50:53 +0200

    fixed flag prefab scale + added sounds

--- Assets/_Project/Scripts/Gameplay/SetFlag.cs
+++ Assets/_Project/Scripts/Gameplay/SetFlag.cs
@@ -1,11 +1,17 @@
-using System;
 using UnityEngine;
 
 public class SetFlag : MonoBehaviour, ICheckpointAnim
 {
     [SerializeField] Animator _anim;
+    [SerializeField] AudioClip _setFlagSound;
 
+    private AudioPlayer _audioPlayer;
     private bool _isCheckpointActivated;
+
+    private void Awake()
+    {
+        _audioPlayer = GetComponent<AudioPlayer>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,6 +19,7 @@ public class SetFlag : MonoBehaviour, ICheckpointAnim
 
         _isCheckpointActivated = true;
         _anim.Play("SetupCheckpoint");
+        _audioPlayer.PlayOneShotSound(_setFlagSound);
     }
     public void ResetToDefaultState()
     {
