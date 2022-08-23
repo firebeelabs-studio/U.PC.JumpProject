1e2fb1479bedff605f1db5c29d551b17fa9e7df6
Adrian Barczewski <adrianbarczewski@gmail.com>
2022-08-23 17:21:16 +0200

    Added Shake Script

--- Assets/_Project/Scripts/Platforms/PlatformRotate.cs
+++ Assets/_Project/Scripts/Platforms/PlatformRotate.cs
@@ -4,38 +4,35 @@ using UnityEngine;
 
 public class PlatformRotate : MonoBehaviour
 {
+    public PlatformShake PlatformShake;
     [SerializeField] private float _timeToRotate = 3f;
     [SerializeField] private float _rotateSpeed = 2f;
     private float _rotateTimer;
     private bool _rotated = false;
-    [SerializeField] float eulerAngZ;
 
     void Update()
     {
-        eulerAngZ = transform.localEulerAngles.z;
         _rotateTimer += Time.deltaTime;
-
-        //Rotate180Deg();
 
         if (_rotateTimer >= _timeToRotate && _rotated == false)
         {
+            PlatformShake.OneShake();
             RotateOnZAxis(180);
-            //_rotated = true;
 
-            if (transform.eulerAngles.z.Equals(180)) //&& _rotated == true)
+            if (transform.eulerAngles.z.Equals(180))
             {
-                //_rotated = false;
                 _rotateTimer = 0;
                 _rotated = true;
             }
         }
+
         if (_rotateTimer >= _timeToRotate && _rotated == true)
         {
+            PlatformShake.OneShake();
             RotateOnZAxis(0);
 
-            if (transform.eulerAngles.z <= 0) //&& _rotated == true)
+            if (transform.eulerAngles.z <= 0)
             {
-                //_rotated = false;
                 _rotateTimer = 0;
                 _rotated = false;
             }
@@ -43,15 +40,8 @@ public class PlatformRotate : MonoBehaviour
         }
     }
 
-    // private void RotatePlatform()
-    // {
-    //     transform.Rotate(0, 0, Mathf.Lerp(0, 90, _rotateSpeed * Time.deltaTime));
-    // }
-
     private void RotateOnZAxis(int degrees)
     {
-        //transform.rotation = Vector3.RotateTowards(transform.rotation, Quaternion.Euler(0,_rotationY,0), 5 * Time.deltaTime);
-        //transform.RotateTowards(transform.rotation, Quaternion.Euler(0,_rotationY,0), 5 * Time.deltaTime);
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, degrees), Time.deltaTime * _rotateSpeed);
     }
 }
--- Assets/_Project/Scripts/Platforms/PlatformShake.cs
+++ Assets/_Project/Scripts/Platforms/PlatformShake.cs
@@ -4,39 +4,106 @@ using UnityEngine;
 
 public class PlatformShake : MonoBehaviour
 {
-    private bool _shaking = false;
-    [SerializeField] private float _shakeAmount;
+    private float _passedAngle = 15.0f;
+    private float _minusAngle = - 15.0f;
+    [SerializeField] private float _speed = 2f;
+    [SerializeField] private float _timeBetweenShakes = 3f;
+    public float Timer;
+    private Quaternion _angleQuaternion;
+    private Quaternion _passedAngleQuaternion;
+    private Quaternion _minusAngleQuaternion;
+    private Quaternion _transformQuaternion;
+    private bool _shakedLeft = false;
+    private bool _shakedRight = false;
+    private bool _zeroAngle = true;
 
-    private void Update()
+    private void Start()
     {
-        if (_shaking)
-        {
-            Vector3 newPos = Random.insideUnitSphere * (Time.deltaTime * _shakeAmount);
-            newPos.y = transform.position.y;
-            newPos.z = transform.position.z;
+        _passedAngleQuaternion = Quaternion.Euler(0, 0, _passedAngle);
+        _minusAngleQuaternion = Quaternion.Euler(0, 0, _minusAngle);
+        _transformQuaternion = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+    }
 
-            transform.position = newPos;
+    private void Update() 
+    {
+        Timer += Time.deltaTime;
+        // if (_timer >= _timeBetweenShakes)
+        // {
+        //     if (_zeroAngle)
+        //     {
+        //         Shake(_passedAngle);
+
+        //         if (transform.rotation.z == _passedAngleQuaternion.z)
+        //         {
+        //             _zeroAngle = false;
+        //             _shakedLeft = true;
+        //         }
+        //     }
+        //     else if (_shakedLeft)
+        //     {
+        //         Shake(_minusAngle);
+
+        //         if (transform.rotation.z == (_minusAngleQuaternion.z))
+        //         {
+        //             _shakedLeft = false;
+        //             _shakedRight = true;
+        //         }
+        //     }
+        //     else if (_shakedRight)
+        //     {
+        //         Shake(0);
+
+        //         if (transform.rotation == Quaternion.Euler(0, 0, 0))
+        //         {
+        //             _shakedRight = false;
+        //             _zeroAngle = true;
+        //             _timer = 0;
+        //         }
+        //     }
+        // }
+    }
+
+    public void OneShake()
+    {
+        if (Timer >= _timeBetweenShakes)
+        {
+            if (_zeroAngle)
+            {
+                Shake(_passedAngle);
+
+                if (transform.rotation.z == _passedAngleQuaternion.z)
+                {
+                    _zeroAngle = false;
+                    _shakedLeft = true;
+                }
+            }
+            else if (_shakedLeft)
+            {
+                Shake(_minusAngle);
+
+                if (transform.rotation.z == (_minusAngleQuaternion.z))
+                {
+                    _shakedLeft = false;
+                    _shakedRight = true;
+                }
+            }
+            else if (_shakedRight)
+            {
+                Shake(0);
+
+                if (transform.rotation == Quaternion.Euler(0, 0, 0))
+                {
+                    _shakedRight = false;
+                    _zeroAngle = true;
+                    Timer = 0;
+                }
+            }
         }
     }
 
-    public void Shake()
+    private void Shake(float degeree)
     {
-        StartCoroutine("ShakeNow");
+        _angleQuaternion = Quaternion.Euler(0, 0, degeree);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, _angleQuaternion, _speed * Time.deltaTime);
     }
-
-    IEnumerator ShakeNow()
-    {
-        Vector3 originalPos = transform.position;
-
-        if (_shaking == false)
-        {
-            _shaking = true;
-        }
-
-        yield return new WaitForSeconds(0.25f);
-
-        _shaking = false;
-        transform.position = originalPos;
-    }
-
 }
