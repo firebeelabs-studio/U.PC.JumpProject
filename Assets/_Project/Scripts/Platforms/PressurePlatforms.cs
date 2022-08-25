4be0c030120716cfc7dd587316b188b9ac513c0d
Adrian Barczewski <adrianbarczewski@gmail.com>
2022-08-25 17:56:41 +0200

    Added pressure plate platforms

--- Assets/_Project/Scripts/Platforms/PressurePlatforms.cs
+++ Assets/_Project/Scripts/Platforms/PressurePlatforms.cs
@@ -7,6 +7,7 @@ public class PressurePlatforms : MonoBehaviour
 {
     [SerializeField] private GameObject _platforms1;
     [SerializeField] private GameObject _platforms2;
+    private bool _platformsActivated;
 
     private void Start()
     {
@@ -18,40 +19,20 @@ public class PressurePlatforms : MonoBehaviour
     {
         if (coll.CompareTag("Player"))
         {
-            switch (2)
+            if (!_platformsActivated)
             {
-                case 1:
-                    PlatformsSetActive(true, _platforms1);
-                    PlatformsSetActive(false, _platforms2);
-                    break;
-
-                case 2:
-                    PlatformsSetActive(false, _platforms1);
-                    PlatformsSetActive(true, _platforms2);
-                    break;
+                PlatformsSetActive(false, _platforms1);
+                PlatformsSetActive(true, _platforms2);
+                _platformsActivated = true;
+            }
+            else if (_platformsActivated)
+            {
+                PlatformsSetActive(true, _platforms1);
+                PlatformsSetActive(false, _platforms2);
+                _platformsActivated = false;
             }
         }
     }
-
-    //private void OnTriggerExit2D(Collider2D coll)
-    //{
-    //    if (coll.CompareTag("Player"))
-    //    {
-    //        switch (2)
-    //        {
-    //            case 1:
-    //                PlatformsSetActive(true, _platforms1);
-    //                PlatformsSetActive(false, _platforms2);
-    //                break;
-
-    //            case 2:
-    //                PlatformsSetActive(false, _platforms1);
-    //                PlatformsSetActive(true, _platforms2);
-    //                break;
-    //        }
-    //    }
-    //}
-
     private void PlatformsSetActive(bool active, GameObject platformsType)
     {
         platformsType.SetActive(active);
