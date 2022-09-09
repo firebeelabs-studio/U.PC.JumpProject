01a92e4119b576bb68c57f7c1906eab7e941893a
Patryk Szela <58202728+5Z3L4@users.noreply.github.com>
2022-09-09 13:13:39 +0200

    new tiles

--- Assets/_Project/Scripts/Skins/SkinCreator.cs
+++ Assets/_Project/Scripts/Skins/SkinCreator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -19,7 +20,7 @@ public class SkinCreator : MonoBehaviour
     {
         var data = SwampieSkin.CreateInstance<SwampieSkin>();
         data.skinType = SwampieSkin.SkinType.Hat;
-        data.Positions = _skinTransforms;
+        data.Positions = _skinTransforms.ToList();
         data.SkinSprite = _sprite;
         string path = $"Assets/_Project/Art/Characters/Skins/{SwampieSkin.SkinType.Jacket.ToString()}/{_name}.asset";
         AssetDatabase.CreateAsset(data, path);
