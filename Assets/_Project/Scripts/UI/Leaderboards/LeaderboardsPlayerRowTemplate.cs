dfe164f3c7f5e99c7cf58a7ed52c61fb33b63720
Krystian Kuska <47749622+OsheOse@users.noreply.github.com>
2023-02-28 22:08:04 +0100

    displaying leaderboards data (no avatars)

--- Assets/_Project/Scripts/UI/Leaderboards/LeaderboardsPlayerRowTemplate.cs
+++ Assets/_Project/Scripts/UI/Leaderboards/LeaderboardsPlayerRowTemplate.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,14 +8,27 @@ using UnityEngine.UI;
 public class LeaderboardsPlayerRowTemplate : MonoBehaviour
 {
     public TMP_Text PlaceText;
-    public Image AvatarImage;
+    public Image BodyImage;
+    public Image HatImage;
+    public Image EyesImage;
+    public Image MouthImage;
+    public Image JacketImage;
     public TMP_Text NicknameText;
     public TMP_Text TimeText;
 
-    public void DisplayData(int place, string nickname, Image avatar, float time)
+    public void DisplayData(LeaderboardEntry leaderboardEntry)
     {
-        PlaceText.text = place.ToString();
-        AvatarImage.sprite = avatar.sprite;
-        NicknameText.text = nickname;
+        PlaceText.text = $"{leaderboardEntry.Rank}.";
+        NicknameText.text = leaderboardEntry.Player.Name;
+        TimeText.text = DisplayTimer(leaderboardEntry.Score);
+    }
+
+    private string DisplayTimer(float time)
+    {
+        string minutes = Math.Floor(time / 60).ToString("00");
+        string seconds = (time % 60).ToString("00");
+        string timer = $"{minutes}:{seconds}";
+        
+        return timer;
     }
 }
---
+++ Assets/_Project/Scripts/UI/Leaderboards/YourScoreLeaderboardsPresenter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YourScoreLeaderboardsPresenter : MonoBehaviour
+{
+    [SerializeField] private LeaderboardsManagerClient _leaderboardsManagerClient;
+    
+
+}
